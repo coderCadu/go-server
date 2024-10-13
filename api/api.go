08bfe8bc8c6dbb02aaf902921c5a051564dbd91c
Carlos Silva <carlosedu.waldorf@gmail.com>
2024-10-12 22:55:58 -0300

    feat: create structs file

--- api/api.go
+++ api/api.go
@@ -6,19 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Clean Architecture", Artist: "coderCadu", Price: 128.92},
-	{ID: "2", Title: "Clean Design", Artist: "coderCadu", Price: 12.91},
-	{ID: "3", Title: "Clean Code", Artist: "coderCadu", Price: 38.97},
-}
-
 func GetAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
